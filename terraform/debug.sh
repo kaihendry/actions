2f26c49992298cbf9ea22f4b55e4261cf8af34cc
Kai Hendry <hendry@iki.fi>
2025-09-14 14:51:38 +0100

    lets apply

--- terraform/debug.sh
+++ terraform/debug.sh
@@ -1,3 +1,4 @@
 REPO=kaihendry/terraform-config
 gh workflow run deploy.yaml -R $REPO
+sleep 5
 gh run view $(gh run list -w deploy.yaml -R $REPO --json databaseId -q '.[0].databaseId') -R $REPO --log-failed
